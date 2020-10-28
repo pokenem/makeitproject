ba3969ff84fece35a6a021f5ddc0afc3d6aa5840
DESKTOP-IKKVNDH\Андрей <andreystepanov0412@gmail.com>
2020-10-29 00:17:48 +0300

    Что-то сделал, не уверен что работает, завтра проверю.

--- makeitproject/makeitproject.ino
+++ makeitproject/makeitproject.ino
@@ -1,20 +1,22 @@
-// пример с "бегущей радугой" для библиотеки FastLED
-#define NUM_LEDS 144
+#define NUM_LEDS 25
 #include "FastLED.h"
-#define PIN 6
+#define PIN 13
 CRGB leds[NUM_LEDS];
 byte counter;
-void setup() {
+void setup()
+{
   FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(50);
   pinMode(13, OUTPUT);
+  Serial.begin(9600); 
 }
-void loop() {
-  for (int i = 0; i < NUM_LEDS; i++ ) {         // от 0 до первой трети
-    leds[i] = CHSV(counter + i * 2, 255, 255);  // HSV. Увеличивать HUE (цвет)
-    // умножение i уменьшает шаг радуги
-  }
-  counter++;        // counter меняется от 0 до 255 (тип данных byte)
+void loop()
+{
+  for (int i = 0; i < NUM_LEDS; i++ ) 
+  {   
+    Serial.read(int r,int g,int b);                 
+    leds[i] = CHSV(r,g,b);              
+  }       
   FastLED.show();
-  delay(5);         // скорость движения радуги
+  delay(5);       
 }
